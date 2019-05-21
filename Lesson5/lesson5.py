292c45b5b1a7dfbc8fac6191af9a510974523f8c
Siva Mulagala <python.shiva@gmail.com>
2019-05-21 12:33:36 +0530

    Stacking unstacking and transposing columns with index

--- Lesson5/lesson5.py
+++ Lesson5/lesson5.py
@@ -7,4 +7,18 @@ i = ['a', 'b']
 
 #Form DataFrame
 df = pd.DataFrame(data= d, index=i)
-print(df)
+# print(df)
+
+#Bring the column and place them in the index
+stack = df.stack()
+print(stack)
+#now it became the multi level index
+# print(stack.index)
+
+unstack = df.unstack()
+print(unstack)
+print(unstack.index)
+
+#We can also flip the column names with the index using Transpose
+transpose = df.T
+print(transpose)
